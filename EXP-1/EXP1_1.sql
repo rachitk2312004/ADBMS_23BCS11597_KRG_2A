140fcf0ff2a7503e031d6d59a4e3d1c6c1f35d0d
rachitk2312004 <149363271+rachitk2312004@users.noreply.github.com>
2025-07-30 16:01:49 +0530

    Add files via upload

---
+++ EXP-1/EXP1_1.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Author (
+  author_id INT PRIMARY KEY,
+  author_name VARCHAR(20),
+  country VARCHAR(20)
+);
+
+CREATE TABLE Book (
+  book_id INT PRIMARY KEY,
+  book_title VARCHAR(20),
+  author_id INT
+);
+
+ALTER TABLE Book
+ADD CONSTRAINT fk_book_author
+FOREIGN KEY (author_id) REFERENCES Author(author_id);
+
+INSERT INTO Author (author_id, author_name, country)
+VALUES
+  (1, 'rachit', 'india'),
+  (2, 'Naman', 'China'),
+  (3, 'Ashu', 'Russia');
+
+INSERT INTO Book (book_id, book_title, author_id)
+VALUES
+  (100, 'science', 1),
+  (200, 'biology', 2),
+  (300, 'English', 1);
+
+SELECT * FROM Author;
+SELECT * FROM Book;
+
+SELECT A.*, B.*
+FROM Author AS A
+INNER JOIN Book AS B ON A.author_id = B.author_id;
